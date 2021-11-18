86455a1b28acbc482fb9a1c1c45f90fb21f58016
jianghaiqiang <jhq0113@163.com>
2021-11-18 14:45:17 +0800

    add byte2float64

--- convert.go
+++ convert.go
@@ -13,3 +13,8 @@ func Bytes2Int64(data []byte) int64 {
 	val, _ := strconv.ParseInt(*(*string)(unsafe.Pointer(&data)), 10, 64)
 	return val
 }
+
+func Bytes2Float64(data []byte) float64 {
+	val, _ := strconv.ParseFloat(*(*string)(unsafe.Pointer(&data)), 64)
+	return val
+}
