91c2ceacd06ee919fd1bbab9565a9a4a5d90da98
jianghaiqiang <jianghaiqiang@360.cn>
2022-11-13 21:32:15 +0800

    add zapkey

--- core/zaplogger/key.go
+++ core/zaplogger/key.go
@@ -1,8 +1,14 @@
 package zaplogger
 
 import (
+	"time"
+
 	"go.uber.org/zap"
 )
+
+func Id(id int64) zap.Field {
+	return zap.Int64("Id", id)
+}
 
 func UpdateAt(updatedAt int64) zap.Field {
 	return zap.Int64("UpdatedAt", updatedAt)
@@ -24,12 +30,32 @@ func Event(name string) zap.Field {
 	return zap.String("Event", name)
 }
 
+func Method(method string) zap.Field {
+	return zap.String("Method", method)
+}
+
+func Path(path string) zap.Field {
+	return zap.String("Path", path)
+}
+
 func Uri(uri string) zap.Field {
 	return zap.String("Uri", uri)
 }
 
+func Duration(duration time.Duration) zap.Field {
+	return zap.Duration("Dration", duration)
+}
+
 func Error(err error) zap.Field {
 	return zap.Error(err)
+}
+
+func Args(args ...interface{}) zap.Field {
+	return zap.Any("Args", args)
+}
+
+func Params(params ...interface{}) zap.Field {
+	return zap.Any("Params", params)
 }
 
 func Value(value interface{}) zap.Field {
