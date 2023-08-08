1d4dcbc22a9606f33b2eac650001e84a75620bfc
jhq0113 <jhq0113@163.com>
2023-08-08 18:04:38 +0800

    query in not int

--- core/query/condition/in.go
+++ core/query/condition/in.go
@@ -29,3 +29,289 @@ func (i In) Build() (sql string, args []interface{}) {
 	buffer.WriteByte(')')
 	return buffer.String(), args
 }
+
+type StringIn struct {
+	Field string   `json:"field"`
+	Value []string `json:"value"`
+}
+
+func (si StringIn) Build() (sql string, args []interface{}) {
+	if len(si.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(si.Field) + 6 + (len(si.Value)-1)*2)
+	args = make([]interface{}, len(si.Value), len(si.Value))
+
+	buffer.WriteString(si.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = si.Value[0]
+	for index := 1; index < len(si.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = si.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type IntIn struct {
+	Field string `json:"field"`
+	Value []int  `json:"value"`
+}
+
+func (ii IntIn) Build() (sql string, args []interface{}) {
+	if len(ii.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ii.Field) + 6 + (len(ii.Value)-1)*2)
+	args = make([]interface{}, len(ii.Value), len(ii.Value))
+
+	buffer.WriteString(ii.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = ii.Value[0]
+	for index := 1; index < len(ii.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ii.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type UintIn struct {
+	Field string `json:"field"`
+	Value []uint `json:"value"`
+}
+
+func (ui UintIn) Build() (sql string, args []interface{}) {
+	if len(ui.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ui.Field) + 6 + (len(ui.Value)-1)*2)
+	args = make([]interface{}, len(ui.Value), len(ui.Value))
+
+	buffer.WriteString(ui.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = ui.Value[0]
+	for index := 1; index < len(ui.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ui.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Int64In struct {
+	Field string  `json:"field"`
+	Value []int64 `json:"value"`
+}
+
+func (ii Int64In) Build() (sql string, args []interface{}) {
+	if len(ii.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ii.Field) + 6 + (len(ii.Value)-1)*2)
+	args = make([]interface{}, len(ii.Value), len(ii.Value))
+
+	buffer.WriteString(ii.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = ii.Value[0]
+	for index := 1; index < len(ii.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ii.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Uint64In struct {
+	Field string   `json:"field"`
+	Value []uint64 `json:"value"`
+}
+
+func (ui Uint64In) Build() (sql string, args []interface{}) {
+	if len(ui.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ui.Field) + 6 + (len(ui.Value)-1)*2)
+	args = make([]interface{}, len(ui.Value), len(ui.Value))
+
+	buffer.WriteString(ui.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = ui.Value[0]
+	for index := 1; index < len(ui.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ui.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Int32In struct {
+	Field string  `json:"field"`
+	Value []int32 `json:"value"`
+}
+
+func (i32i Int32In) Build() (sql string, args []interface{}) {
+	if len(i32i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(i32i.Field) + 6 + (len(i32i.Value)-1)*2)
+	args = make([]interface{}, len(i32i.Value), len(i32i.Value))
+
+	buffer.WriteString(i32i.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = i32i.Value[0]
+	for index := 1; index < len(i32i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = i32i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Uint32In struct {
+	Field string   `json:"field"`
+	Value []uint32 `json:"value"`
+}
+
+func (u32i Uint32In) Build() (sql string, args []interface{}) {
+	if len(u32i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(u32i.Field) + 6 + (len(u32i.Value)-1)*2)
+	args = make([]interface{}, len(u32i.Value), len(u32i.Value))
+
+	buffer.WriteString(u32i.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = u32i.Value[0]
+	for index := 1; index < len(u32i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = u32i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Int16In struct {
+	Field string  `json:"field"`
+	Value []int16 `json:"value"`
+}
+
+func (i16i Int16In) Build() (sql string, args []interface{}) {
+	if len(i16i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(i16i.Field) + 6 + (len(i16i.Value)-1)*2)
+	args = make([]interface{}, len(i16i.Value), len(i16i.Value))
+
+	buffer.WriteString(i16i.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = i16i.Value[0]
+	for index := 1; index < len(i16i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = i16i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Uint16In struct {
+	Field string   `json:"field"`
+	Value []uint16 `json:"value"`
+}
+
+func (u16i Uint16In) Build() (sql string, args []interface{}) {
+	if len(u16i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(u16i.Field) + 6 + (len(u16i.Value)-1)*2)
+	args = make([]interface{}, len(u16i.Value), len(u16i.Value))
+
+	buffer.WriteString(u16i.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = u16i.Value[0]
+	for index := 1; index < len(u16i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = u16i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Int8In struct {
+	Field string `json:"field"`
+	Value []int8 `json:"value"`
+}
+
+func (i8i Int8In) Build() (sql string, args []interface{}) {
+	if len(i8i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(i8i.Field) + 6 + (len(i8i.Value)-1)*2)
+	args = make([]interface{}, len(i8i.Value), len(i8i.Value))
+
+	buffer.WriteString(i8i.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = i8i.Value[0]
+	for index := 1; index < len(i8i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = i8i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type Uint8In struct {
+	Field string  `json:"field"`
+	Value []uint8 `json:"value"`
+}
+
+func (u8i Uint8In) Build() (sql string, args []interface{}) {
+	if len(u8i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(u8i.Field) + 6 + (len(u8i.Value)-1)*2)
+	args = make([]interface{}, len(u8i.Value), len(u8i.Value))
+
+	buffer.WriteString(u8i.Field)
+	buffer.WriteString(" IN(?")
+	args[0] = u8i.Value[0]
+	for index := 1; index < len(u8i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = u8i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
--- core/query/condition/not_in.go
+++ core/query/condition/not_in.go
@@ -27,3 +27,289 @@ func (ni NotIn) Build() (sql string, args []interface{}) {
 	buffer.WriteByte(')')
 	return buffer.String(), args
 }
+
+type NotStringIn struct {
+	Field string   `json:"field"`
+	Value []string `json:"value"`
+}
+
+func (nsi NotStringIn) Build() (sql string, args []interface{}) {
+	if len(nsi.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nsi.Field) + 10 + (len(nsi.Value)-1)*2)
+	args = make([]interface{}, len(nsi.Value), len(nsi.Value))
+
+	buffer.WriteString(nsi.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nsi.Value[0]
+	for index := 1; index < len(nsi.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nsi.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotIntIn struct {
+	Field string `json:"field"`
+	Value []int  `json:"value"`
+}
+
+func (nii NotIntIn) Build() (sql string, args []interface{}) {
+	if len(nii.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nii.Field) + 10 + (len(nii.Value)-1)*2)
+	args = make([]interface{}, len(nii.Value), len(nii.Value))
+
+	buffer.WriteString(nii.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nii.Value[0]
+	for index := 1; index < len(nii.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nii.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotUintIn struct {
+	Field string `json:"field"`
+	Value []uint `json:"value"`
+}
+
+func (nui NotUintIn) Build() (sql string, args []interface{}) {
+	if len(nui.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nui.Field) + 10 + (len(nui.Value)-1)*2)
+	args = make([]interface{}, len(nui.Value), len(nui.Value))
+
+	buffer.WriteString(nui.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nui.Value[0]
+	for index := 1; index < len(nui.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nui.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotInt64In struct {
+	Field string  `json:"field"`
+	Value []int64 `json:"value"`
+}
+
+func (ni64i NotInt64In) Build() (sql string, args []interface{}) {
+	if len(ni64i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ni64i.Field) + 10 + (len(ni64i.Value)-1)*2)
+	args = make([]interface{}, len(ni64i.Value), len(ni64i.Value))
+
+	buffer.WriteString(ni64i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = ni64i.Value[0]
+	for index := 1; index < len(ni64i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ni64i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotUint64In struct {
+	Field string   `json:"field"`
+	Value []uint64 `json:"value"`
+}
+
+func (nu64i NotUint64In) Build() (sql string, args []interface{}) {
+	if len(nu64i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nu64i.Field) + 10 + (len(nu64i.Value)-1)*2)
+	args = make([]interface{}, len(nu64i.Value), len(nu64i.Value))
+
+	buffer.WriteString(nu64i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nu64i.Value[0]
+	for index := 1; index < len(nu64i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nu64i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotInt32In struct {
+	Field string  `json:"field"`
+	Value []int32 `json:"value"`
+}
+
+func (ni32i NotInt32In) Build() (sql string, args []interface{}) {
+	if len(ni32i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ni32i.Field) + 10 + (len(ni32i.Value)-1)*2)
+	args = make([]interface{}, len(ni32i.Value), len(ni32i.Value))
+
+	buffer.WriteString(ni32i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = ni32i.Value[0]
+	for index := 1; index < len(ni32i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ni32i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotUint32In struct {
+	Field string   `json:"field"`
+	Value []uint32 `json:"value"`
+}
+
+func (nu32i NotUint32In) Build() (sql string, args []interface{}) {
+	if len(nu32i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nu32i.Field) + 10 + (len(nu32i.Value)-1)*2)
+	args = make([]interface{}, len(nu32i.Value), len(nu32i.Value))
+
+	buffer.WriteString(nu32i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nu32i.Value[0]
+	for index := 1; index < len(nu32i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nu32i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotInt16In struct {
+	Field string  `json:"field"`
+	Value []int16 `json:"value"`
+}
+
+func (ni16i NotInt16In) Build() (sql string, args []interface{}) {
+	if len(ni16i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ni16i.Field) + 10 + (len(ni16i.Value)-1)*2)
+	args = make([]interface{}, len(ni16i.Value), len(ni16i.Value))
+
+	buffer.WriteString(ni16i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = ni16i.Value[0]
+	for index := 1; index < len(ni16i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ni16i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotUint16In struct {
+	Field string   `json:"field"`
+	Value []uint16 `json:"value"`
+}
+
+func (nu16i NotUint16In) Build() (sql string, args []interface{}) {
+	if len(nu16i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nu16i.Field) + 10 + (len(nu16i.Value)-1)*2)
+	args = make([]interface{}, len(nu16i.Value), len(nu16i.Value))
+
+	buffer.WriteString(nu16i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nu16i.Value[0]
+	for index := 1; index < len(nu16i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nu16i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotInt8In struct {
+	Field string `json:"field"`
+	Value []int8 `json:"value"`
+}
+
+func (ni8i NotInt8In) Build() (sql string, args []interface{}) {
+	if len(ni8i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(ni8i.Field) + 10 + (len(ni8i.Value)-1)*2)
+	args = make([]interface{}, len(ni8i.Value), len(ni8i.Value))
+
+	buffer.WriteString(ni8i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = ni8i.Value[0]
+	for index := 1; index < len(ni8i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = ni8i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
+
+type NotUint8In struct {
+	Field string  `json:"field"`
+	Value []uint8 `json:"value"`
+}
+
+func (nu8i NotUint8In) Build() (sql string, args []interface{}) {
+	if len(nu8i.Value) == 0 {
+		return
+	}
+
+	var buffer strings.Builder
+	buffer.Grow(len(nu8i.Field) + 10 + (len(nu8i.Value)-1)*2)
+	args = make([]interface{}, len(nu8i.Value), len(nu8i.Value))
+
+	buffer.WriteString(nu8i.Field)
+	buffer.WriteString(" NOT IN(?")
+	args[0] = nu8i.Value[0]
+	for index := 1; index < len(nu8i.Value); index++ {
+		buffer.WriteString(",?")
+		args[index] = nu8i.Value[index]
+	}
+
+	buffer.WriteByte(')')
+	return buffer.String(), args
+}
